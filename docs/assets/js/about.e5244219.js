65ea8975d47e45a1b788ef4dbd71dee01b0f28d8
Luke Carl Thompson <lukecarlthompson@gmail.com>
2019-11-13 21:47:27 +0800

    copy dist files to docs folder

---
+++ docs/assets/js/about.e5244219.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["about"],{f820:function(t,e,n){"use strict";n.r(e);var c=function(){var t=this,e=t.$createElement;t._self._c;return t._m(0)},s=[function(){var t=this,e=t.$createElement,n=t._self._c||e;return n("div",{staticClass:"account"},[n("h1",[t._v("This is the account page")])])}],u=n("2877"),a={},i=Object(u["a"])(a,c,s,!1,null,null,null);i.options.__file="About.vue";e["default"]=i.exports}}]);
+//# sourceMappingURL=about.e5244219.js.map
